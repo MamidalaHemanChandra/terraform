456b8b5d1794c2351f1cde30b1ca6ea4cc360580
heman chandra <hemanchandra701@gmail.com>
2025-10-17 20:24:13 +0530

    count

--- count/r53.tf
+++ count/r53.tf
@@ -1,7 +1,8 @@
 resource "aws_route53_record" "roboshop" {
-  zone_id = Z00303221M2EO78HUMVX6
+  zone_id = var.zone_id
   name    = "${var.instances[count.index].$(domain_name)}"
   type    = "A"
   ttl     = 1
   records = [var.instances[count.index].private_ip]
+  allow_overwrite = true
 }
