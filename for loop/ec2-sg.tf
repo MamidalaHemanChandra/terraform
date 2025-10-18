5ed25b5159aa6a75a06ccbf64a52f11ada68c6b2
heman chandra <hemanchandra701@gmail.com>
2025-10-18 19:17:49 +0530

    r53

---
+++ for loop/ec2-sg.tf
@@ -0,0 +1,35 @@
+resource "aws_instance" "terraform" {
+  for_each = var.instances
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = each.value
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+  tags = {
+    Name = each.key
+    Terraform = "true"
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow-all"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+ 
+
+  tags = {
+    Name = "allow-all"
+  }
+}
---
+++ for loop/variables.tf
@@ -0,0 +1,17 @@
+variable "instances" {
+    # default = [ "mongodb","redis", "mysql" ]
+    default = {
+        mongodb : "t3.micro"
+        redis : "t3.micro"
+        mysql : "t3.small"
+    }
+}
+
+variable "zone_id" {
+    default = "Z00303221M2EO78HUMVX6"
+}
+
+variable "domain_name" {
+    default = "heman.icu"
+}
+
