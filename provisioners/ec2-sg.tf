e9e3a874b4714c224bac0150f1a7ffa872fceb4b
heman chandra <hemanchandra701@gmail.com>
2025-10-26 13:58:03 +0530

    locals

---
+++ provisioners/ec2-sg.tf
@@ -0,0 +1,47 @@
+resource "aws_instance" "terraform" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+  tags = {
+    Name = "terraform"
+    Terraform = "true"
+  }
+
+  provisioners "local-exec" {
+    command = "echo ${self.private_ip} > inventory"
+    onfailure = continue
+  }
+
+  provisioners "local-exec" {
+    command = "Instance is destroyed"
+    when = destroy
+  }
+
+}
+
+
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow-all"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+ 
+
+  tags = {
+    Name = "allow-all"
+  }
+}
